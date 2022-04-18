55e673078f2df29ca34f1c024ef490e94c034540
cx33 <lne128gene980@163.com>
2022-04-18 14:22:54 +0800

    solved: 898

---
+++ 898-easy-includes/template.ts
@@ -0,0 +1,12 @@
+type Includes<T extends readonly any[], U> =
+  T extends [] ? false :
+  T extends [infer First, ...infer Rest]
+  ? (Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>)
+  : false
+
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
