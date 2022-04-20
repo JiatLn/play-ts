6c4f51f18c380ae2464319b8a5a7e048f550ed98
cx33 <lne128gene980@163.com>
2022-04-20 17:21:01 +0800

    update: 119

--- medium/119-medium-replaceall/template.ts
+++ medium/119-medium-replaceall/template.ts
@@ -1,8 +1,9 @@
 type ReplaceAll<S extends string, From extends string, To extends string> =
   From extends '' ? S :
-  S extends `${infer s}${From}${infer e}`
+  S extends `${infer left}${From}${infer right}`
   // 这里递归要递归其它字符串，已匹配的不能再次计算
-  ? `${ReplaceAll<s, From, To>}${To}${ReplaceAll<e, From, To>}`
+  // left 已经没有了！！！不用再匹配
+  ? `${left}${To}${ReplaceAll<right, From, To>}`
   : S
 
 type t = ReplaceAll<'foobarfoobar', 'ob', 'b'>
