e9e70c629c8806d4c9e340377f6d67839f8cc101
cx33 <lne128gene980@163.com>
2022-04-21 22:05:52 +0800

    solved: 599

---
+++ medium/599-medium-merge/template.ts
@@ -0,0 +1,17 @@
+type Merge<F, S> = {
+  [key in keyof F | keyof S]:
+  key extends keyof S
+  ? S[key]
+  : key extends keyof F ? F[key] : never
+}
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
+
+type cases = Merge<Foo, Bar>
