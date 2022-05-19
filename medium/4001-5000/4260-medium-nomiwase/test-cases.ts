8faa1f28541a8818fb27ef47bf77591fe23ef70f
cx33 <lne128gene980@163.com>
2022-05-19 23:28:25 +0800

    update: 4260

--- medium/4001-5000/4260-medium-nomiwase/test-cases.ts
+++ medium/4001-5000/4260-medium-nomiwase/test-cases.ts
@@ -1,7 +1,9 @@
-// type AllCombinations<S extends string, T extends string = ''> =
-//   S extends `${infer a}${infer b}`
-//   ? AllCombinations<b, `${a}` | `${T}` | `${a}${T}` | `${T}${a}`>
-//   : T
+type AllCombinations<S extends string, PRE extends string = ''> =
+  S extends `${infer a}${infer b}`
+  ?
+  | `${a}${AllCombinations<`${PRE}${b}`>}`
+  | AllCombinations<b, `${PRE}${a}`>
+  : ''
 
 // https://github.com/type-challenges/type-challenges/issues/5339
 type String2Array<S extends string, N extends any[] = []>
@@ -13,14 +15,19 @@ type String2Union<S extends string> =
   : never;
 
 type test1 = String2Union<'ABC'>
-type test2 = AllCombinations<'ABCD'>
-
-type AllCombinations<S extends string, U extends string = String2Union<S>> =
-  [U] extends [never]
-  ? ''
-  : '' | { [K in U]: `${K}${AllCombinations<never, Exclude<U, K>>}` }[U]
+type test2 = AllCombinations<'ABC'>
 
 
+// type AllCombinations<S extends string, U extends string = String2Union<S>> =
+//   [U] extends [never]
+//   ? ''
+//   : '' | { [K in U]: `${K}${AllCombinations<never, Exclude<U, K>>}` }[U]
+
+
+type UnionAllCombinations<S extends string> = [S] extends [never] ? ''
+  : '' | { [K in S]: `${K}${UnionAllCombinations<Exclude<S, K>>}` }[S]
+
+// type AllCombinations<S extends string> = UnionAllCombinations<String2Union<S>>
 
 
 
