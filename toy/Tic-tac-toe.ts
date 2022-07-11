d436300483a3316197edf3cf1ab19d8cfc0488de
林佳涛 <lne128gene980@163.com>
2022-07-11 17:12:48 +0800

    feat: add check game status

--- toy/Tic-tac-toe.ts
+++ toy/Tic-tac-toe.ts
@@ -7,6 +7,8 @@ type Grid<T extends Chess = '⬜'> = {
   chess: T;
 }
 
+type WonShape<T extends Chess> = [Grid<T>, Grid<T>, Grid<T>]
+
 type Row<
   G1 extends Grid<Chess> = Grid,
   G2 extends Grid<Chess> = Grid,
@@ -55,26 +57,65 @@ type SetChess<B extends
     : SetChess<B, RN, CN, C, [...Result, B[CurrRow['length']]], [...CurrRow, any]>)
 
 
+type CheckGameStatus<B extends
+  Board<
+    Row<Grid<Chess>, Grid<Chess>, Grid<Chess>>,
+    Row<Grid<Chess>, Grid<Chess>, Grid<Chess>>,
+    Row<Grid<Chess>, Grid<Chess>, Grid<Chess>>
+  >> = B extends [infer R1, infer R2, infer R3]
+  ? WonShape<'❌'> extends R1 | R2 | R3 ? 'The wonner is ❌!'
+  : [B[0][0], B[1][1], B[2][2]] extends WonShape<'❌'> ? 'The wonner is ❌!'
+  : [B[0][2], B[1][1], B[2][0]] extends WonShape<'❌'> ? 'The wonner is ❌!'
+  : [B[0][0], B[1][0], B[2][0]] extends WonShape<'❌'> ? 'The wonner is ❌!'
+  : [B[0][1], B[1][1], B[2][1]] extends WonShape<'❌'> ? 'The wonner is ❌!'
+  : [B[0][2], B[1][2], B[2][2]] extends WonShape<'❌'> ? 'The wonner is ❌!'
+  : WonShape<'⭕'> extends R1 | R2 | R3 ? "The wonner is ⭕!"
+  : [B[0][0], B[1][1], B[2][2]] extends WonShape<'⭕'> ? "The wonner is ⭕!"
+  : [B[0][2], B[1][1], B[2][0]] extends WonShape<'⭕'> ? "The wonner is ⭕!"
+  : [B[0][0], B[1][0], B[2][0]] extends WonShape<'⭕'> ? "The wonner is ⭕!"
+  : [B[0][1], B[1][1], B[2][1]] extends WonShape<'⭕'> ? "The wonner is ⭕!"
+  : [B[0][2], B[1][2], B[2][2]] extends WonShape<'⭕'> ? "The wonner is ⭕!"
+  : 'The game continue.'
+  : never
+
+
+
+
 type NewGame = Board
 type renderGame0 = RenderBoard<NewGame>
 
 type step1 = SetChess<NewGame, 1, 1, '❌'>
 type renderGame1 = RenderBoard<step1>
+type status1 = CheckGameStatus<step1>
 
 type step2 = SetChess<step1, 2, 1, '⭕'>
 type renderGame2 = RenderBoard<step2>
+type status2 = CheckGameStatus<step2>
 
 type step3 = SetChess<step2, 1, 0, '❌'>
 type renderGame3 = RenderBoard<step3>
+type status3 = CheckGameStatus<step3>
 
 type step4 = SetChess<step3, 1, 2, '⭕'>
 type renderGame4 = RenderBoard<step4>
+type status4 = CheckGameStatus<step4>
 
 type step5 = SetChess<step4, 2, 0, '❌'>
 type renderGame5 = RenderBoard<step5>
+type status5 = CheckGameStatus<step5>
 
 type step6 = SetChess<step5, 0, 0, '⭕'>
 type renderGame6 = RenderBoard<step6>
+type status6 = CheckGameStatus<step6>
 
 type step7 = SetChess<step6, 0, 2, '❌'>
 type renderGame7 = RenderBoard<step7>
+type status7 = CheckGameStatus<step7>
+
+
+type NewGame2 = Board
+type step01 = SetChess<NewGame2, 1, 1, '⭕'>
+type step02 = SetChess<step01, 1, 2, '⭕'>
+type step03 = SetChess<step02, 1, 0, '⭕'>
+type renderGame02 = RenderBoard<step03>
+type status44 = CheckGameStatus<step03> // "The wonner is ⭕!"
